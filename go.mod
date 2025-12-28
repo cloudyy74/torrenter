cda46fd7333d23294905c455c3676e56ff6f2423
Vitas Remeykis <ccloudyy74@gmail.com>
2025-12-28 12:27:50 +0300

    feat: peer unmarshal

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cloudyy74/torrenter
 
 go 1.25.5
+
+require github.com/jackpal/bencode-go v1.0.2 // indirect
---
+++ internal/peer/peer.go
@@ -0,0 +1,30 @@
+package peer
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+)
+
+// Peer encodes connection information for a peer
+type Peer struct {
+	IP   net.IP
+	Port uint16
+}
+
+// Unmarshal parses peer IP addresses and ports from a buffer
+func Unmarshal(peersBin []byte) ([]Peer, error) {
+	const peerSize = 6 // 4 for IP, 2 for port
+	if len(peersBin)%peerSize != 0 {
+		return nil, fmt.Errorf("received malformed peers of length: %d", len(peersBin))
+	}
+
+	numPeers := len(peersBin) / peerSize
+	peers := make([]Peer, numPeers)
+	for i := range numPeers {
+		offset := i * peerSize
+		peers[i].IP = net.IPv4(peersBin[offset], peersBin[offset+1], peersBin[offset+2], peersBin[offset+3])
+		peers[i].Port = binary.BigEndian.Uint16(peersBin[offset+4: offset+6])
+	}
+	return peers, nil
+}
