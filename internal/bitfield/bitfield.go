8a7c610f7223680a359f7cead228b66e302d0877
Vitas Remeykis <ccloudyy74@gmail.com>
2025-12-30 12:05:33 +0300

    feat: bitfield improvements + tests

--- internal/bitfield/bitfield.go
+++ internal/bitfield/bitfield.go
@@ -7,12 +7,24 @@ type Bitfield []byte
 func (bf Bitfield) HasPiece(index int) bool {
 	byteIndex := index / 8
 	offset := index % 8
-	return bf[byteIndex]>>(7-offset)&1 != 0
+
+	// silently discard invalid bounded index
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return false
+	}
+
+	return bf[byteIndex]>>uint(7-offset)&1 != 0
 }
 
 // SetPiece sets a bit in the bitfield
 func (bf Bitfield) SetPiece(index int) {
 	byteIndex := index / 8
 	offset := index % 8
-	bf[byteIndex] |= 1 << (7 - offset)
+
+	// silently discard invalid bounded index
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return
+	}
+
+	bf[byteIndex] |= 1 << uint(7 - offset)
 }
---
+++ internal/bitfield/bitfield_test.go
@@ -0,0 +1,79 @@
+package bitfield
+
+import (
+	"reflect"
+	"testing"
+)
+
+type hasPieceTest struct {
+	input Bitfield
+	want  []bool    // want[i] is a result for input.HasPiece(i)
+}
+
+type setPieceTest struct {
+	input Bitfield
+	index int
+	want  Bitfield
+}
+
+func TestHasPiece(t *testing.T) {
+	tests := []hasPieceTest {
+		{
+			input: Bitfield{},
+			want: []bool{false, false , false},
+		},
+		{
+			input: Bitfield{0b10100110},
+			want: []bool{true, false, true, false, false, true, true, false, false, false},
+		},
+		{
+			input: Bitfield{0b01010100, 0b01010100},
+			want: []bool{false, true, false, true, false, true, false, false, false, true, false, true, false, true, false, false, false, false, false, false},
+		},
+
+	}
+
+	for _, tt := range tests {
+		resultLen := len(tt.want)
+		result := make([]bool, resultLen)
+		for i := range resultLen {
+			result[i] = tt.input.HasPiece(i)
+		}
+		if !reflect.DeepEqual(result, tt.want) {
+			t.Errorf("bitfield: %v, wanted: %v, got: %v", tt.input, tt.want, result)
+		}
+	}
+}
+
+func TestSetPiece(t *testing.T) {
+	tests := []setPieceTest {
+		{
+			input: Bitfield{0b01010100, 0b01010100},
+			index: 4, //                             v (set)
+			want: Bitfield{0b01011100, 0b01010100},
+		},
+		{
+			input: Bitfield{0b01010100, 0b01010100},
+			index: 9, //                             v (noop)
+			want: Bitfield{0b01010100, 0b01010100},
+		},
+		{
+			input: Bitfield{0b01010100, 0b01010100},
+			index: 15, //                            v (set)
+			want: Bitfield{0b01010100, 0b01010101},
+		},
+		{
+			input: Bitfield{0b01010100, 0b01010100},
+			index: 19, //                            v (noop)
+			want: Bitfield{0b01010100, 0b01010100},
+		},
+	}
+
+	for _, tt := range tests {
+		bf := append(Bitfield{}, tt.input...)
+		bf.SetPiece(tt.index)
+		if !reflect.DeepEqual(bf, tt.want) {
+			t.Errorf("bitfield: %v, index: %d, wanted: %v, got: %v", tt.input, tt.index, tt.want, bf)
+		}
+	}
+}
