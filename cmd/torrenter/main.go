2378d0bf305c78fad946862daf3acd91dd263fa6
Vitas Remeykis <ccloudyy74@gmail.com>
2026-01-02 09:01:11 +0300

    feat: main.go

--- cmd/torrenter/main.go
+++ cmd/torrenter/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/cloudyy74/torrenter/internal/torrentfile"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %s <path to torrent> <output path>", os.Args[0])
+		return
+	}
+	inPath := os.Args[1]
+	outPath := os.Args[2]
+
+	tf, err := torrentfile.Open(inPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := tf.DownloadToFile(outPath); err != nil {
+		log.Fatal(err)
+	}
 }
